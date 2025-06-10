2c44e2a00582d62565cdfe58dbc23cc65b928465
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-10 10:07:14 +0300

    main navbar code

---
+++ Bookito/Bookito/src/Navbar.jsx
@@ -0,0 +1,14 @@
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <a href="/">MyApp</a>
+      </div>
+      <ul className="navbar__links">
+        <li className="navbar__item">
+          <a href="/" className="navbar__link">Books</a>
+          <a href="/" className="navbar__link">AudioBooks</a>
+    </nav>
+)
+}
