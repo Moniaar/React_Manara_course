0f55c0c6d17c6f58752a6d96910e83d2edcd9b08
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-10 10:20:23 +0300

    main navbar code is now fixed

--- Bookito/Bookito/src/Navbar.jsx
+++ Bookito/Bookito/src/Navbar.jsx
@@ -3,12 +3,15 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
+        <p> testing if this works </p>
         <a href="/">MyApp</a>
       </div>
       <ul className="navbar__links">
         <li className="navbar__item">
           <a href="/" className="navbar__link">Books</a>
           <a href="/" className="navbar__link">AudioBooks</a>
+        </li>
+      </ul>
     </nav>
-)
+  )
 }
