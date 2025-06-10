9fc2cc89da5606ff6dec85f29c80bba97ce6e302
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-10 10:20:37 +0300

    added the right navbar component

--- Bookito/Bookito/src/App.jsx
+++ Bookito/Bookito/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import {Navbar} from './components/Navbar'
+import {Navbar} from './Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Navbar />
     </>
   )
 }
