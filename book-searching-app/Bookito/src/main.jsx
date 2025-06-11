7b73f69a42578cc0839362e8feb72d471dd48156
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-11 20:03:43 +0300

    added HMR

---
+++ book-searching-app/Bookito/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+// It keeps the application state even though the source code changes.
+// The application itself
+reloads, but not the page.
+if (model.hot) {
+  model.hot.accpet ;
+}
