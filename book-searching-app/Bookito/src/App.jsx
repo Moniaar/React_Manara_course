a7ce90715fb01e0c4839eefc55f2bffc556d22ac
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-11 20:26:17 +0300

    this has a rendered list with the map function now

--- book-searching-app/Bookito/src/App.jsx
+++ book-searching-app/Bookito/src/App.jsx
@@ -2,13 +2,47 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar'
 
+const list = [ 
+  {
+  author: 'Omnia',
+  url: 'https://github.com/Moniaar',
+  title: 'My GitHub',
+  description: 'Check out my GitHub profile for projects and contributions.',
+  },
+  {
+  title: 'My Portfolio',
+  url: 'https://github.com/Moniaar/Official-Portfolio',
+  author: 'Omnia',
+  description: 'Explore my portfolio showcasing my work and skills.',
+  },
+];
+
+
 function App() {
   const [count, setCount] = useState(0)
-  var hello = ' hi from above here hehe ';
+  var hello = ' sir you fine? ';
+  const list = [ 
+  {
+  author: 'Omnia',
+  url: 'https://github.com/Moniaar',
+  title: 'My GitHub',
+  description: 'Check out my GitHub profile for projects and contributions.',
+  },
+  {
+  title: 'My Portfolio',
+  url: 'https://github.com/Moniaar/Official-Portfolio',
+  author: 'Omnia',
+  description: 'Explore my portfolio showcasing my work and skills.',
+  },
+];
+
   return ( 
     <>
       <Navbar />
       <h1>{hello}</h1>
+      {list.map(function(item) {
+        return <div>{item.title}</div>;
+        })}
     </>
   )
 }
