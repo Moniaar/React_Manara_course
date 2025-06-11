67b1ba591e56a49da027c251f6231aaddc848eab
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-11 20:37:24 +0300

    added links

--- book-searching-app/Bookito/src/App.jsx
+++ book-searching-app/Bookito/src/App.jsx
@@ -21,27 +21,14 @@ const list = [
 function App() {
   const [count, setCount] = useState(0)
   var hello = ' sir you fine? ';
-  const list = [ 
-  {
-  author: 'Omnia',
-  url: 'https://github.com/Moniaar',
-  title: 'My GitHub',
-  description: 'Check out my GitHub profile for projects and contributions.',
-  },
-  {
-  title: 'My Portfolio',
-  url: 'https://github.com/Moniaar/Official-Portfolio',
-  author: 'Omnia',
-  description: 'Explore my portfolio showcasing my work and skills.',
-  },
-];
-
   return ( 
     <>
       <Navbar />
       <h1>{hello}</h1>
       {list.map(function(item) {
-        return <div>{item.title}</div>;
+        return (
+            <span><a href={item.url}>{item.title}</a></span>
+        );
         })}
     </>
   )
