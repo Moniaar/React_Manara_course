9ee453fe9d00607a1d80b6a759e4b053b19bf114
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-14 13:05:56 +0300

    added classes

--- book-searching-app/Bookito/src/App.jsx
+++ book-searching-app/Bookito/src/App.jsx
@@ -17,17 +17,29 @@ const list = [
   },
 ];
 
+class User {
+  constructor(name, age) {
+  this.name = name;
+  this.age = age;
+  }
+  getName() {
+    return this.name + ' is ' + this.age + ' years old';
+  }
 
 function App() {
   const [count, setCount] = useState(0)
   var hello = ' sir you fine? ';
+  const user = new User('Omnia', 25);
+  console.log(user.getName());
   return ( 
     <>
       <Navbar />
       <h1>{hello}</h1>
       {list.map(function(item) {
         return (
-            <span><a href={item.url}>{item.title}</a></span>
+            <div key={item.objectID}>
+              <span><a href={item.url}>{item.title}</a></span>
+            </div>
         );
         })}
     </>
